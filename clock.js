3e1accbdae6f86b901c6b20c24af73be951aed03
Kai Love <ferretbeansqwq@gmail.com>
2023-07-25 15:19:33 +0800

    changed fonts and fixed some stuff :3

--- clock.js
+++ clock.js
@@ -1,6 +1,5 @@
-const date = new Date();
-console.log(date.toString());
-document.getElementById("time").innerHTML = date.toString().replace("GMT+0800 (Australian Western Standard Time)", "");
 setInterval(() => {
-    location.reload();
-}, 100);
+    const date = new Date();
+    console.log(date.toString());
+    document.getElementById("time").innerHTML = date.toString().replace("GMT+0800 (Australian Western Standard Time)", "");
+}, 1000);
